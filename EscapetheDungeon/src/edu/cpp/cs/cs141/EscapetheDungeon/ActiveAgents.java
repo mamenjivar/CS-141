3ba628b679c131f87008076c85abfe3483cedb8b
mamenjivar <Miguel@b98-wc1-auth-134-71-115-58.wlan.cpp.edu>
2016-04-27 11:05:38 -0700

    Assignment #3

---
+++ EscapetheDungeon/src/edu/cpp/cs/cs141/EscapetheDungeon/ActiveAgents.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package edu.cpp.cs.cs141.EscapetheDungeon;
+
+/**
+ * @author Miguel Menjivar
+ *
+ */
+public class ActiveAgents {
+
+}
---
+++ EscapetheDungeon/src/edu/cpp/cs/cs141/EscapetheDungeon/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.cpp.cs.cs141.EscapetheDungeon;
+
+/**
+ * @author Miguel Menjivar
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
