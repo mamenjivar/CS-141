e8d3f48b5b45f62ebc2e06da36aaa9ee72cd0539
mamenjivar <menjivarmiguel221@gmail.com>
2016-05-01 16:45:28 -0700

    This has been updated for the project

---
+++ EscapetheDungeon/src/edu/cpp/cs/cs141/EscapetheDungeon/TestEnemy.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package edu.cpp.cs.cs141.EscapetheDungeon;
+
+/**
+ * @author User
+ *
+ */
+public class TestEnemy {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Enemy enemy = new Enemy();
+		enemy.Hit();
+		enemy.Hit();
+
+	}
+
+}
