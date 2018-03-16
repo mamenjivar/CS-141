a16ac67b4ec0e1bcf9327108f297cd8f86f3b273
mamenjivar <menjivarmiguel221@gmail.com>
2018-03-16 16:13:11 -0700

    updating rest of files for target practice

--- TargetPractice/src/targetPractice/Main.java
+++ TargetPractice/src/targetPractice/Main.java
@@ -1,27 +1,23 @@
 /**
  * CS 141: Intro to Programming and Problem Solving
- * Professor: Edwin Rodríguez
  * 
  * Programming Assignment #2
  * 
- * This assignment is based off target practice. The purpose is that the user will
+ * The purpose is that the user will
  * be able to choose a weapon ranging from hand guns, rifles, and shotguns. They also
  * have to shoot at five targets.
  * 
- * Miguel Menjivar
- */
-package edu.cpp.cs.cs141.targetpractice;
-
-/**
  * @author Miguel Menjivar
  * 
- * Runs the program which begins at the user interface class
- *
+ * Main: Runs the program
  */
+package targetPractice;
+
 public class Main {
 	public static void main(String[] args){
-		UserInterface test = new UserInterface();
-		test.Intro();
+		//UserInterface test = new UserInterface();
+		GameEngine gE = new GameEngine();
+		gE.start();
 	}
 
 }
